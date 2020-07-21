3c52586dfcc8625f7091f8533778fc1042c5a1bd
mmorales056 <mmorales056@misena.edu.co>
2020-07-21 11:04:56 -0500

    first commit

---
+++ Sigma_api/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Sigma_api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sigma_api.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly string connectionStrig = "server=178.128.146.252;user id=admin_sigmauser;password=pfaDKIJyPF;port=3306;database=admin_sigmatest;";
+
+        [HttpPost]
+        public IActionResult AddContact(Contacts contact)
+        {
+            int result = 0;
+            using (var connection = new MySqlConnection(connectionStrig))
+            {
+                var sql = "insert into contacts (name,email,state,city)" + " values (@name,@email,@state,@city)";
+                result = connection.Execute(sql, contact);
+            }
+
+            return Ok(result);
+
+        }
+    }
+
+    }
---
+++ Sigma_api/Model/Contacts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sigma_api.Model
+{
+    public class Contacts
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+    }
+}
